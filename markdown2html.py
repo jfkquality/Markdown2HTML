becdf2b5cb57126f1fea5444599b12ce5015bf4b
jfkquality <jfkquality@gmail.com>
2020-05-03 02:45:21 -0400

    Task 0 Debug. Add if __name__ == __main__.

--- markdown2html.py
+++ markdown2html.py
@@ -5,12 +5,13 @@ import sys
 
 
 # def readinput(argv=None):
-if (len(sys.argv)) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    exit(1)
+if __name__ == "__main__":
+    if (len(sys.argv)) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
 
-if sys.argv[1] != "README.md":
-    print("Missing README.md", file=sys.stderr)
-    exit(1)
+        if sys.argv[1] != "README.md":
+            print("Missing README.md", file=sys.stderr)
+            exit(1)
 
-exit(0)
+            exit(0)
