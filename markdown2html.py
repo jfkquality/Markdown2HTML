41abc43e39b60984c8a1322683534ec933ce5da2
jfkquality <jfkquality@gmail.com>
2020-05-03 02:47:37 -0400

    Task 0 Debug. Linter errors 2: line 10 too long.

--- markdown2html.py
+++ markdown2html.py
@@ -7,7 +7,8 @@ import sys
 # def readinput(argv=None):
 if __name__ == "__main__":
     if (len(sys.argv)) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         exit(1)
 
         if sys.argv[1] != "README.md":
