bd4e0a98fdc3719d2ad6ad7ab492a30490d81bad
jfkquality <jfkquality@gmail.com>
2020-05-03 02:36:09 -0400

    Task 0 Debug. Linter errors.

--- markdown2html.py
+++ markdown2html.py
@@ -5,10 +5,10 @@ from sys import argv, stderr
 # def readinput(argv=None):
 if (len(argv)) < 3:
     print("Usage: ./markdown2html.py README.md README.html", file=stderr)
-    exit (1)
+    exit(1)
 
 if argv[1] != "README.md":
     print("Missing README.md", file=stderr)
-    exit (1)
+    exit(1)
 
-exit (0)
+exit(0)
