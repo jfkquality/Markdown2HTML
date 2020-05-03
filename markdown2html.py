6bdf4a2390173c901bbd953bcaaae8c576e39b68
jfkquality <jfkquality@gmail.com>
2020-05-03 02:39:13 -0400

    Task 0 Debug. Precede sys methods with 'sys.'.

--- markdown2html.py
+++ markdown2html.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-from sys import argv, stderr
+import sys
 
 
 # def readinput(argv=None):
-if (len(argv)) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=stderr)
+if (len(sys.argv)) < 3:
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     exit(1)
 
-if argv[1] != "README.md":
-    print("Missing README.md", file=stderr)
+if sys.argv[1] != "README.md":
+    print("Missing README.md", file=sys.stderr)
     exit(1)
 
 exit(0)
