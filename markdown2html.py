b4dd95c05a10e0909212726c7072900152e703c6
jfkquality <jfkquality@gmail.com>
2020-05-03 15:34:07 -0400

    Task 0 Debug: __main__ reads argv, passes to function (*args).

--- markdown2html.py
+++ markdown2html.py
@@ -4,14 +4,18 @@
 from __future__ import print_function
 import sys
 
-if __name__ == "__main__":
-    if (len(sys.argv)) < 3:
+def getinput(*args):
+    myargs = args[0]
+    if len(myargs) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
         exit(1)
 
-    if sys.argv[1] != "README.md":
+    if myargs[1] != "README.md":
         print("Missing README.md", file=sys.stderr)
         exit(1)
-
     exit(0)
+
+
+if __name__ == "__main__":
+    getinput(sys.argv)
