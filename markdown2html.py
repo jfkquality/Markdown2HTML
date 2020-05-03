d55a67d7eec0dbf2164fbdce3dd5537bf2b97a2b
jfkquality <jfkquality@gmail.com>
2020-05-03 18:11:35 -0400

    Task 0 Debug: Change back to not using a function.

--- markdown2html.py
+++ markdown2html.py
@@ -6,20 +6,21 @@ import sys
 from  pathlib import Path
 
 
-def getinput(*args):
-    """ Parse input and test."""
-    myargs = args[0]
-    if len(myargs) < 3:
+# def getinput(*args):
+""" Parse input and test."""
+# myargs = *args  #args[0]
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
         exit(1)
 
-    if myargs[1] != "README.md" or not Path(myargs[1]).is_file():
+    if sys.argv[1] != "README.md" or not Path(sys.argv[1]).is_file():
         print("Missing README.md", file=sys.stderr)
         exit(1)
     exit(0)
 
 
-if __name__ == "__main__":
-    """Main function"""
-    getinput(sys.argv)
+# if __name__ == "__main__":
+#     """Main function"""
+#     getinput(sys.argv)
