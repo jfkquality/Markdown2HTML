f7c0bcd015361a73137b3a86532ef0ab3544be83
jfkquality <jfkquality@gmail.com>
2020-05-03 02:52:58 -0400

    Task 0 Debug. Wrong indenting after name = main if.

--- markdown2html.py
+++ markdown2html.py
@@ -3,16 +3,14 @@
 
 import sys
 
-
-# def readinput(argv=None):
 if __name__ == "__main__":
     if (len(sys.argv)) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
         exit(1)
 
-        if sys.argv[1] != "README.md":
-            print("Missing README.md", file=sys.stderr)
-            exit(1)
+    if sys.argv[1] != "README.md":
+        print("Missing README.md", file=sys.stderr)
+        exit(1)
 
-            exit(0)
+        exit(0)
