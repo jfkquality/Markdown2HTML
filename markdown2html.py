2bb0fdbda0105cd7eac5802c0e806f04eb2b124d
jfkquality <jfkquality@gmail.com>
2020-05-03 23:28:18 -0400

    Task 0 Debug: Totally rewrite in a new file.

--- markdown2html.py
+++ markdown2html.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Check for markdown file."""
 
-from sys import argv, stderr
-from os import path
+"""
+This module converts markdown to html
+"""
+
+
 if __name__ == "__main__":
+    from sys import argv, stderr
+    from os import path
     if len(argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=stderr)
         exit(1)
     if not path.exists(argv[1]):
         print("Missing {}".format(argv[1]), file=stderr)
         exit(1)
-    exit(0)
