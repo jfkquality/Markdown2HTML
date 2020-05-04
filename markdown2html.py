f7916d6abed7d01c61a3ce77c20283b3beb89131
jfkquality <jfkquality@gmail.com>
2020-05-03 23:20:20 -0400

    Task 0 Debug: Totally rewrite.

--- markdown2html.py
+++ markdown2html.py
@@ -1,26 +1,13 @@
 #!/usr/bin/python3
 """Check for markdown file."""
 
-import sys
+from sys import argv, stderr
 from os import path
-
-# def getinput(*args):
-# """ Parse input and test."""
-# myargs = *args  #args[0]
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html",
-              file=sys.stderr)
+    if len(argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=stderr)
         exit(1)
-
-    # if sys.argv[1] != "README.md" or not Path(sys.argv[1]).is_file():
-    # if not Path(sys.argv[1]).is_file():
-    if not path.exists(sys.argv[1]):
-        print("Missing {}".format(sys.argv[1]), file=sys.stderr)
+    if not path.exists(argv[1]):
+        print("Missing {}".format(argv[1]), file=stderr)
         exit(1)
-    # exit(0)
-
-
-# if __name__ == "__main__":
-#     """Main function"""
-#     getinput(sys.argv)
+    exit(0)
